47e09c4434caa6a7845b4fca2522ade97b78d7e4
Valeriy Kechin <valeraghst@gmail.com>
2022-11-27 19:50:47 +0400

    Prepare for bot: 1. remove verx server (I forgot that telegram bots don't need an explicit server...) 2. add org.telegram.telegrambots lib #5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 
 plugins {
     kotlin("jvm") version "1.7.20"
-    application
-    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "me.valer"
 version = "1.0-SNAPSHOT"
 
-val vertxVersion = "4.3.5"
-val junitJupiterVersion = "5.7.0"
 
-val mainVerticleName = "server.ServerVert"
-val launcherClassName = "io.vertx.core.Launcher"
-
-val watchForChange = "src/**/*"
-val doOnChange = "${projectDir}/gradlew classes"
-
-application {
-    mainClass.set(launcherClassName)
-}
+val tgApi = "6.3.0"
 
 repositories {
     mavenCentral()
@@ -31,32 +17,9 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.20")
     testImplementation(kotlin("test"))
-    implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
-    implementation("io.vertx:vertx-web")
-    implementation("io.vertx:vertx-lang-kotlin")
-    testImplementation("io.vertx:vertx-junit5")
-    testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+    implementation("org.telegram:telegrambots:$tgApi")
 }
 
 
 val compileKotlin: KotlinCompile by tasks
 compileKotlin.kotlinOptions.jvmTarget = "1.8"
-
-tasks.withType<ShadowJar> {
-    archiveClassifier.set("fat")
-    manifest {
-        attributes(mapOf("Main-Verticle" to mainVerticleName))
-    }
-    mergeServiceFiles()
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events = setOf(PASSED, SKIPPED, FAILED)
-    }
-}
-
-tasks.withType<JavaExec> {
-    args = listOf("run", mainVerticleName, "--redeploy=$watchForChange", "--launcher-class=$launcherClassName", "--on-redeploy=$doOnChange")
-}
