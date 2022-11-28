e9753e42b55d467b3e87de4d2e7ea85a5385dcc2
Valeriy Kechin <valeraghst@gmail.com>
2022-11-28 17:26:11 +0400

    Added voice download #2 #3

--- src/main/kotlin/Bot.kt
+++ src/main/kotlin/Bot.kt
@@ -1,14 +1,16 @@
+import org.apache.commons.io.FileUtils
 import org.telegram.abilitybots.api.bot.AbilityBot
 import org.telegram.abilitybots.api.objects.Reply
 import org.telegram.abilitybots.api.objects.ReplyFlow
 import org.telegram.abilitybots.api.util.AbilityUtils.getChatId
+import org.telegram.telegrambots.meta.api.methods.GetFile
 import org.telegram.telegrambots.meta.api.objects.Update
 import java.io.File
+import java.net.URL
 import java.sql.Connection
 import java.sql.DriverManager
 import java.util.*
 import java.util.function.Predicate
-
 
 class Bot(val token: String, val username: String) : AbilityBot(token, username) {
     private lateinit var conn: Connection
@@ -67,6 +69,19 @@ class Bot(val token: String, val username: String) : AbilityBot(token, username)
             .build()
     }
 
+    fun readVoiceFlow(): ReplyFlow {
+        return ReplyFlow.builder(db)
+            .action { _, upd ->
+                val msg = readVoice(upd.message.voice.fileId)
+                silent.send(
+                    msg,
+                    getChatId(upd)
+                )
+            }
+            .onlyIf(hasVoice())
+            .build()
+    }
+
     private fun addKey(chatId: String, vkKey: String) {
         val st = conn.prepareStatement("INSERT INTO keys(chatId, key) VALUES (?, ?) " +
                 "ON CONFLICT (chatId) DO UPDATE SET key=EXCLUDED.key;")
@@ -81,6 +96,16 @@ class Bot(val token: String, val username: String) : AbilityBot(token, username)
         st.setString(1, chatId)
         st.executeUpdate()
         st.close()
+    }
+
+    private fun readVoice(fileId: String): String {
+        val uploadedFile = GetFile()
+        uploadedFile.fileId = fileId
+        val file = execute(uploadedFile)
+        val localFile = File("localVoice/$fileId.ogg")
+        val inpStr = URL(file.getFileUrl(token)).openStream()
+        FileUtils.copyInputStreamToFile(inpStr, localFile)
+        return "aaa"
     }
 
     override fun creatorId(): Long {
@@ -106,6 +131,12 @@ class Bot(val token: String, val username: String) : AbilityBot(token, username)
         return Predicate { upd: Update ->
             !upd.message.text.equals("/addkey", ignoreCase = true) && (upd.message.text.length <= 11
                     || upd.message.text.length >= 200 || upd.message.text.contains(' '))
+        }
+    }
+
+    private fun hasVoice(): Predicate<Update> {
+        return Predicate { upd: Update ->
+            upd.message.hasVoice()
         }
     }
 }
